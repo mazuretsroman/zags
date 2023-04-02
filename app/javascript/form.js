47ca7483b09abcf81829b69842ea1a2d80704d3e
Roman Mazurets <mazurets.roma@gmail.com>
2023-04-03 02:17:49 +0300

    toggle partner selectors on form

--- app/javascript/form.js
+++ app/javascript/form.js
@@ -1,9 +1,48 @@
 document.addEventListener('turbo:load', function() {
   const personForm = document.getElementById('js-person-form');
+
   if (personForm) {
     const wifeSelector = document.getElementById('js-wife-selector');
     const husbandSelector = document.getElementById('js-husband-selector');
-    const genderSelector = document.getElementById('js-gender-selector');
+    const genderSelector = document.getElementById('person_gender');
+    const marriageDateSelector = document.getElementById('js-marriage-date-selector');
+    const wifeSelectInput = document.getElementById('person_wife_id');
+    const husbandSelectInput = document.getElementById('person_husband_id');
+
+    const togglePartnerSelectors = () => {
+      if (genderSelector.value == 'm') {
+        husbandSelector.classList.add('d-none');
+        wifeSelector.classList.remove('d-none');
+        husbandSelectInput.value = '';
+      } else {
+        husbandSelector.classList.remove('d-none');
+        wifeSelector.classList.add('d-none');
+        wifeSelectInput.value = '';
+      }
+    }
+
+    const toggleMarriageDate = () => {
+      const isWife = wifeSelectInput.value != '';
+      const isHusband = husbandSelectInput.value != '';
+
+      if (isWife || isHusband) {
+        marriageDateSelector.classList.remove('d-none');
+      } else {
+        marriageDateSelector.classList.add('d-none');
+      }
+    }
+
+    togglePartnerSelectors(); 
+    toggleMarriageDate();
+    genderSelector.addEventListener('change', function() {
+      togglePartnerSelectors(); 
+    });
+    husbandSelector.addEventListener('change', function() {
+      toggleMarriageDate();
+    });
+    wifeSelector.addEventListener('change', function() {
+      toggleMarriageDate();
+    });
   }
 })
 
